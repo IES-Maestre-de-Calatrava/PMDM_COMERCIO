cd3487fa5430bf98cedc4c530e73e80033496846
MAlejandroGP <199757279+MAlejandroGP@users.noreply.github.com>
2026-01-26 12:45:45 +0100

    Preview de AyudaActivity

--- app/src/main/java/es/maestre/juntosjc/AyudaActivity.kt
+++ app/src/main/java/es/maestre/juntosjc/AyudaActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import es.maestre.juntosjc.ui.theme.JUNTOSJCTheme
 
@@ -73,8 +74,12 @@ fun AyudaPrincipal(){
         }
 
     }
+}
 
-
+@Preview
+@Composable
+fun AyudaPrincipalPreview(){
+    AyudaPrincipal()
 }
 
 @Composable
