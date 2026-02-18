9aa16f383f4ee090afd5a64858a608237955ee60
Manuel Alejandro García Pineño <garciapinenomanuelalejandro@gmail.com>
2026-02-18 19:27:35 +0100

    Agregar bordes y cambiar padding
    Agregado un borde a los botones principales y cambiados varios márgenes

--- app/src/main/java/es/maestre/juntosjc/AyudaActivity.kt
+++ app/src/main/java/es/maestre/juntosjc/AyudaActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,6 +22,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -356,8 +358,8 @@ fun BotonTexto(contenido: String, texto: String) {
     var expandido by remember { mutableStateOf(false) }
 
     Column(
-        modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
-        horizontalAlignment = Alignment.Start
+        modifier = Modifier.fillMaxWidth().padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         OutlinedButton(
             onClick = { expandido = !expandido },
@@ -388,7 +390,7 @@ fun BotonTexto(contenido: String, texto: String) {
             enter = expandVertically() + fadeIn(),
             exit = shrinkVertically() + fadeOut()
         ) {
-            androidx.compose.material3.Card(
+            Card(
                 modifier = Modifier
                     .padding(vertical = 4.dp)
                     .fillMaxWidth(),
@@ -397,7 +399,7 @@ fun BotonTexto(contenido: String, texto: String) {
             ) {
                 Text(
                     text = texto,
-                    modifier = Modifier.padding(12.dp),
+                    modifier = Modifier.padding(4.dp),
                     style = MaterialTheme.typography.bodyMedium
                 )
             }
@@ -409,6 +411,7 @@ fun BotonTexto(contenido: String, texto: String) {
 val ColumnaPrincipalModifier = Modifier
     .fillMaxWidth()
     .padding(horizontal = 16.dp, vertical = 8.dp)
+    .border(1.dp, Color.Gray, shape = RoundedCornerShape(8.dp))
 
 val ColumnaDetalleModifier = Modifier
     .padding(start = 16.dp, top = 4.dp)
